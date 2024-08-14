99d0174f91f7b547350adf95fdabafdade8bca6d
thestateofcybersecurity <98850458+thestateofcybersecurity@users.noreply.github.com>
2024-08-14 09:11:12 -0600

    Create categories.js

---
+++ categories.js
@@ -0,0 +1,35 @@
+document.getElementById('cissp-category').addEventListener('click', function() {
+    window.location.href = 'cissp.html';
+});
+
+document.getElementById('ceh-category').addEventListener('click', function() {
+    window.location.href = 'ceh.html';
+});
+
+document.getElementById('cybersecurity-category').addEventListener('click', function() {
+    window.location.href = 'cybersecurity.html';
+});
+
+document.getElementById('securityplus-category').addEventListener('click', function() {
+    window.location.href = 'securityplus.html';
+});
+
+document.getElementById('chfi-category').addEventListener('click', function() {
+    window.location.href = 'chfi.html';
+});
+
+document.getElementById('gsec-category').addEventListener('click', function() {
+    window.location.href = 'gsec.html';
+});
+
+document.getElementById('sscp-category').addEventListener('click', function() {
+    window.location.href = 'sscp.html';
+});
+
+document.getElementById('lpt-category').addEventListener('click', function() {
+    window.location.href = 'lpt.html';
+});
+
+document.getElementById('cysa-category').addEventListener('click', function() {
+    window.location.href = 'cysa.html';
+});
